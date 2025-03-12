bdaf0c42cd0acbd8224bc1f77fa829e89536306a
Kassim <kassimrooney@gmail.com>
2025-03-12 20:24:19 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = '31' * '2';
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 32;
+const num4 = 7;
+
+const mod = '32' % '7';
+
+const max = Math.max(5, 10, 15, 20);
